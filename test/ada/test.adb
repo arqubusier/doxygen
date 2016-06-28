f7828daf0bd8dba427ed6b8ece4719743dd305f1
Herman Lundkvist <herman.lundkvist@saabgroup.com>
2016-06-28 09:31:03 +0200

    Normal comments working

--- test/ada/test.adb
+++ test/ada/test.adb
@@ -1,4 +1,9 @@
---!\file \brief this is a special comment
+--! \file test.adb
+--! \brief this is a special comment.
+--!
+--! detailed description
+
+-- oetnuhoneuh
 with Ada.Text_IO; use Ada.Text_IO;
     procedure Hello is
     begin
